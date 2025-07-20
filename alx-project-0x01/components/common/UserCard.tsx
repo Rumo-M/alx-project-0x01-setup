b2248ea60b2585c2c880668ad61d657bb83a55c6
Martha Maloba <marthamarumo92@gmail.com>
2025-07-20 14:07:19 +0200

    Update the UserCard.tsx, index.tsx iside users and also the interfaces/index.ts

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
+import { UserProps } from '@/interfaces'; // ✅ import { UserProps } from
 
-type UserCardProps = {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  company?: {
-    name: string;
-  };
-};
-
-const UserCard: React.FC<UserCardProps> = ({ id, name, username, email, company }) => {
+const UserCard: React.FC<UserProps> = ({ id, name, username, email, address, company }: UserProps) => { // ✅ <UserProps>
   return (
     <div className="p-4 border rounded shadow bg-white">
       <h2 className="text-lg font-bold mb-1">{name}</h2>
       <p className="text-sm text-gray-600">@{username}</p>
       <p className="text-sm text-gray-700">Email: {email}</p>
-      {company && (
-        <p className="text-sm text-gray-500">Company: {company.name}</p>
-      )}
+      <p className="text-sm text-gray-500">Address: {address.street}, {address.city}</p>
+      <p className="text-sm text-gray-500">Company: {company.name}</p>
+      <p className="text-sm text-gray-500 italic">"{company.catchPhrase}"</p>
       <p className="text-xs text-gray-400 mt-2">User ID: {id}</p>
     </div>
   );
