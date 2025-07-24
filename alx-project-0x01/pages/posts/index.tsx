9b5d24d69cd03ec0b99425be5e9ac09cf77db9e3
Martha Maloba <marthamarumo92@gmail.com>
2025-07-24 20:20:14 +0200

    Modify index.tsx

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,16 +1,13 @@
-// pages/posts/index.tsx
-
-import Header from "@/components/layout/Header";
-import { GetStaticProps } from "next";
-import { PostProps } from "@/interfaces";
-import PostCard from "@/components/common/PostCard";
+import { GetStaticProps } from 'next';
+import { PostProps } from '@/interfaces';
+import PostCard from '@/components/common/PostCard';
 
 interface PostsPageProps {
   posts: PostProps[];
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const posts: PostProps[] = await res.json();
 
   return {
@@ -21,7 +18,6 @@ export const getStaticProps: GetStaticProps = async () => {
 const Posts = ({ posts }: PostsPageProps) => {
   return (
     <div className="p-4">
-      <Header />
       <h1 className="text-2xl font-bold mb-4">Posts</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {posts.map((post) => (
